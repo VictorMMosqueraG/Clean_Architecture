bf9d7e214888981128e9617b01e35e33f48ceb9b
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-04-16 15:30:08 -0500

    Application was created
    i did create a Dto in application

---
+++ src/main/java/clean/architecture/cleanarchitecture/application/dto/CreateBookDto.java
@@ -0,0 +1,21 @@
+package clean.architecture.cleanarchitecture.application.dto;
+
+public class CreateBookDto {
+    
+    private String tittle;
+    private String description;
+
+    // Getters and Setters
+    public String getTittle() {
+        return tittle;
+    }
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }    
+}
