b0cbea4eb2fbb553aba6f589c14d3b8e067f0959
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-04-16 15:13:19 -0500

    First commit

---
+++ src/test/java/clean/architecture/cleanarchitecture/CleanarchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package clean.architecture.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
