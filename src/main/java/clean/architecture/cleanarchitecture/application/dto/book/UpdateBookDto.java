82b9fbb2e94aa8394095f79d7826c354ccae0439
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-04-19 11:44:37 -0500

    Refactor DTO
    was created a baseBookDto and bookCreateDto and bookUpdateDto will extends

--- src/main/java/clean/architecture/cleanarchitecture/application/dto/book/UpdateBookDto.java
+++ src/main/java/clean/architecture/cleanarchitecture/application/dto/book/UpdateBookDto.java
@@ -1,37 +1,9 @@
 package clean.architecture.cleanarchitecture.application.dto.book;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Size;
+import clean.architecture.cleanarchitecture.application.dto.book.base.BaseBookDto;
 
-public class UpdateBookDto {
+
+public class UpdateBookDto  extends BaseBookDto{
     
-    @Size(
-        min = 5, 
-        max = 255, 
-        message = "tittle must be between 5 and 255 characters"
-    )
-    @Schema(example = "Book Tittle Update")
-    private String tittle;
-
-    @Size(
-        min = 5, 
-        max = 255, 
-        message = "description must be between 5 and 255 characters"
-    )
-    @Schema(example = "Book Description Update")
-    private String description;
-
-    // Getters and Setters
-    public String getTittle() {
-        return tittle;
-    }
-    public void setTittle(String tittle) {
-        this.tittle = tittle;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }   
+   
 }
---
+++ src/main/java/clean/architecture/cleanarchitecture/application/dto/book/base/BaseBookDto.java
@@ -0,0 +1,42 @@
+package clean.architecture.cleanarchitecture.application.dto.book.base;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class BaseBookDto {
+    
+    @Size(
+        min = 5,
+        max = 255,
+        message = "tittle must be between 5 and 255 characters"
+    )
+    @NotBlank(message = "Tittle is required, it cannot be blank or null")
+    @Schema(example = "Book Tittle")
+    private String tittle;
+
+    @Size(
+        min = 5,
+        max = 255,
+        message = "description must be between 5 and 255 characters"
+    )
+    @NotBlank(message = "Description is required, it cannot be blank or null")
+    @Schema(example = "Book Description")
+    private String description;
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
